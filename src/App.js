657f08273a9761c8889e3f09b7d095756187c8de
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-21 21:42:06 +0700

    fix: update landingpage slicing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<LandingPage/>}></Route>
+    </Routes>
   )
 }
 
